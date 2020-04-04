66eb6e6c3bc9efa5e1ba3756e86e0e15c5a83a88
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-04 11:25:08 -0400

    sign show

--- app/controllers/sun_signs_controller.rb
+++ app/controllers/sun_signs_controller.rb
@@ -1,2 +1,8 @@
 class SunSignsController < ApplicationController
+    skip_before_action :authorized, only: [:show]
+    
+    def show
+        sign = SunSign.find_by(id: params[:id])
+        render json: sign.to_json()
+    end
 end
